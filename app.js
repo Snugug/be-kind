e37294446a18ef3fe4edfbf948331edc4dc7fb32
Sam Richard <snugug@gmail.com>
2015-09-12 08:07:09 -0400

    Add sample tone analysis

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 'use strict';
 
 var express = require('express'),
+    watson = require('watson-developer-cloud'),
     cfenv = require('cfenv');
 
 var app = express(),
-    appEnv = cfenv.getAppEnv();
+    appEnv = cfenv.getAppEnv(),
+    credentials = {};
 
+if (process.env.VCAP_SERVICES) {
+  credentials = JSON.parse(process.env.VCAP_SERVICES).tone_analyzer[0].credentials;
+}
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/analyze', function (req, res) {
+  var toneAnalyzer = watson.tone_analyzer({
+    'username': credentials.username,
+    'password': credentials.password,
+    'version': 'v2'
+  });
+
+  toneAnalyzer.tone({
+    'text': "I've got a lovely bunch of coconuts. Here they are a standing in a row. Big ones, small ones, some as big as your head! Give 'em a twist, a flick of the wrist, that's what the showman said."
+  }, function (err, tone) {
+    if (err) {
+      res.send(JSON.stringify(err));
+    }
+    else {
+      res.send(JSON.stringify(tone));
+    }
+  });
+});
 
 app.listen(appEnv.port, function() {
   console.log('Server starting on ' + appEnv.url);
